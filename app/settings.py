34fd0d4477fe3205b61e21752f4ff4457f6e85e7
Rafael Neves <Rafaelldasneves@hotmail.com>
2025-10-29 02:02:33 -0300

    fazendo deploy

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +14,7 @@ SECRET_KEY = 'django-insecure-tb7!m*1)klz)0k5c*e5dr147$8k+l2#(+wut25q#qpukhay-sg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rafaelneves.pythonanywhere.com', '*']
 
 
 # Application definition
@@ -108,6 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
